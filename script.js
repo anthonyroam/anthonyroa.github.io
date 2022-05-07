de72bec99d87d20853b56c63a9f7956fdbb90944
anthonyroam <anthonyroam2@gmail.com>
2022-05-07 03:18:47 -0400

    Dark/light mode version 1

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const screen = document.querySelector(".screen");
+const previous = document.querySelector(".previous");
 const buttons = document.querySelector(".buttons");
+const screenMode = document.querySelector(".screen__mode");
+const btn = document.querySelectorAll(".btn");
+const screenContainer = document.querySelector(".main__container")
+
+screenMode.addEventListener("click", ()=> {
+    buttons.classList.toggle("buttons--lm");
+    btn.forEach(button => {
+        button.classList.toggle("btn--lm");
+    });
+    screenContainer.classList.toggle("screen__container--lm");
+})
 
 buttons.addEventListener("click", (e)=> {
     const button = e.target;
     const buttonData = button.dataset;
-    if (buttonData.operator) {
+    if (buttonData.number) {
+        calculator.writeNumber(buttonData.number);
+    } else if (buttonData.operator) {
         calculator.writeOperator(buttonData.operator);
     } else if (buttonData.equals) {
         calculator.calculate();
-    } else if (buttonData.clear) {
+    } else if (buttonData.clearall) {
         calculator.clearAll();
-    } else if (buttonData.number) {
-        calculator.writeNumber(buttonData.number);
+    } else if (buttonData.clear) {
+        calculator.clear();
     }
 });
 
@@ -22,49 +36,61 @@ const calculator = {
     result: "",
    
     writeNumber(button) {
-        screen.textContent += button; 
+        if (button === ".") {
+            if (screen.textContent.indexOf(".") !== -1) return
+        };
+        
         this.number = button;
+        screen.textContent += button; 
     },
 
     writeOperator(operator) {
-        if (screen.textContent === "") return; 
-
-        if (this.hasOperator) {
+        
+        if (screen.textContent === "" && previous.textContent === "") return; 
+        
+        if (this.operator !== undefined) {
             this.calculate();
-
-            if (this.hasOperator) {
+            if (this.operator !== undefined) {
                 this.operator = operator;
-
-                let slice = screen.textContent.slice(0, -1);
-                screen.textContent= (slice += operator);
                 
-                return this.number1, this.operator;
+                let sliceOperator = previous.textContent.slice(0, -1);
+                previous.textContent = (sliceOperator += operator);
+                
+                return this.operator;
             }
         }
-
+        
         this.operator = operator;
-        screen.textContent += operator
+        previous.textContent = screen.textContent;
+        previous.textContent += operator;
+        screen.textContent = "";
 
         return this.operator
     },   
 
     set number(number) {
-        if (this.hasOperator) return this.number2 += number;
+        if (this.operator !== undefined) return this.number2 += number;
         else return this.number1 += number;       
     },
 
     get hasOperator() {
-        if (screen.textContent.indexOf("+") !== -1) return true;
-        else if (screen.textContent.indexOf("-") !== -1) return true;
-        else if (screen.textContent.indexOf("x") !== -1) return true;
+        if (previous.textContent.indexOf("+") !== -1) return true;
+        else if (previous.textContent.indexOf("-") !== -1) return true;
+        else if (previous.textContent.indexOf("x") !== -1) return true;
         else return false;
     },
 
     clearAll() {
         screen.textContent = "";
+        previous.textContent = "";
         this.number1 = "";
         this.number2 = "";
-        this.operator = "";
+        this.operator = undefined;
+    },
+
+    clear() {
+        let deleteLast = screen.textContent.slice(0,-1);
+        screen.textContent = deleteLast;
     },
 
     calculate() {
@@ -73,9 +99,9 @@ const calculator = {
         // console.log( `operator: ${this.operator}`)
 
         if (this.number1 === "" || this.number2 === "") return;
-        
-        this.number1 = parseInt(this.number1);
-        this.number2 = parseInt(this.number2);
+
+        this.number1 = parseFloat(this.number1);
+        this.number2 = parseFloat(this.number2);
 
         switch (this.operator) {
             case "+":
@@ -87,13 +113,18 @@ const calculator = {
             case "x":
                 this.result = this.number1 * this.number2;
                 break;
-                
+            case "÷":
+                this.result = this.number1 / this.number2;
+                break;
             default:
                 break;
         }
+        // previous.textContent = `${this.number1}${this.operator}${this.number2}=`
+
+        previous.textContent = "";
         screen.textContent = this.result;
         this.number1 = this.result;
         this.number2 = "";
-        this.operator = "";
+        this.operator = undefined;
     },
 };
