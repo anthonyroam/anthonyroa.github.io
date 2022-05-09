ab85915737c4c86b90dbd8127eef71984bed877c
anthonyroam <anthonyroam2@gmail.com>
2022-05-09 17:05:44 -0400

    version Final Calculadora.js

--- script.js
+++ script.js
@@ -1,142 +1,132 @@
-const screen = document.querySelector(".screen");
-const previous = document.querySelector(".previous");
+const currentScreen = document.querySelector(".screen");
+const previousScreen = document.querySelector(".previous");
 const buttons = document.querySelector(".buttons");
-const screenMode = document.querySelector(".screen__mode");
+const screenMode = document.querySelector(".screen__switch");
 
 screenMode.addEventListener("click", ()=> {
     document.body.classList.toggle("active");
-
 })
 
 buttons.addEventListener("click", (e)=> {
     const button = e.target;
     const buttonData = button.dataset;
     if (buttonData.number) {
-        calculator.writeNumber(buttonData.number);
+        calculator.appendNumber(buttonData.number);
+        calculator.updateScreen();
     } else if (buttonData.operator) {
-        calculator.writeOperator(buttonData.operator);
+        calculator.chooseOperator(buttonData.operator);
+        calculator.updateScreen();
     } else if (buttonData.equals) {
         calculator.calculate();
+        calculator.updateScreen();
     } else if (buttonData.clearall) {
         calculator.clearAll();
+        calculator.updateScreen();
     } else if (buttonData.clear) {
         calculator.clear();
-    }
+        calculator.updateScreen();
+    } else if (buttonData.switch) {
+        calculator.switch();
+        calculator.updateScreen();
+    };
 });
 
 const calculator = {
-    number1: "",
-    number2: "",
-    operator: undefined,
+    currentNumber: "",
+    previousNumber: "",
     result: "",
+    operator: undefined,
    
-    writeNumber(button) {
-        if (button === ".") {
-            if (screen.textContent.indexOf(".") !== -1) return
-        };
-        
-        this.number = button;
-        screen.textContent += button; 
+    appendNumber(button) {
+        if (button === "." && this.currentNumber.toString().includes(".")) return;
+        this.currentNumber += button;        
     },
+    
+    chooseOperator(operator) {
+        if (this.currentNumber === "" && this.previousNumber === "") return;
+        if (this.currentNumber === "" && this.previousNumber !== "") return this.operator = operator;
+        if (this.operator !== undefined) this.calculate();
 
-    writeOperator(operator) {
-        
-        // if (screen.textContent === "" && previous.textContent === "") return; 
-        if (screen.textContent === "") {
-            if (operator === "-") {
-                screen.textContent = operator;
-                return this.number = operator;
-            }; 
-        }
-
-        if (screen.textContent === "-") return;
-        
-        if (this.operator !== undefined) {
-            this.calculate();
-            if (this.operator !== undefined) {
-                this.operator = operator;
-                
-                let sliceOperator = previous.textContent.slice(0, -1);
-                previous.textContent = (sliceOperator += operator);
-                
-                return this.operator;
-            }
-        }
-        
         this.operator = operator;
-        previous.textContent = screen.textContent;
-        previous.textContent += operator;
-        screen.textContent = "";
-
-        return this.operator
+        this.previousNumber = this.currentNumber;
+        this.currentNumber = "";
     },   
 
-    set number(number) {
-        if (this.operator !== undefined) return this.number2 += number;
-        else return this.number1 += number;       
+    getDisplayNumber(number) {
+        let numberString = number.toString();
+        if (numberString.indexOf(".") === -1) return numberString.replace(/(\d)(?=(\d{3})+\b)/g,'$1,');
+       
+        const integerDigits = numberString.split(".")[0];
+        const decimalDigits = numberString.split(".")[1];
+        let integerDisplay
+        integerDisplay = integerDigits.replace(/(\d)(?=(\d{3})+\b)/g,'$1,');
+        if (decimalDigits !== null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
     },
 
-    get hasOperator() {
-        if (previous.textContent.indexOf("+") !== -1) return true;
-        else if (previous.textContent.indexOf("-") !== -1) return true;
-        else if (previous.textContent.indexOf("x") !== -1) return true;
-        else return false;
+    updateScreen() {
+        currentScreen.textContent = this.getDisplayNumber(this.currentNumber);
+
+        if (this.operator !== undefined) {
+            previousScreen.textContent = `${this.getDisplayNumber(this.previousNumber)}${this.operator}`;
+        } else previousScreen.textContent = "";
     },
 
     clearAll() {
-        screen.textContent = "";
-        previous.textContent = "";
-        this.number1 = "";
-        this.number2 = "";
+        this.currentNumber = "";
+        this.previousNumber = "";
         this.operator = undefined;
     },
 
     clear() {
-        let deleteLast = screen.textContent.slice(0,-1);
-        screen.textContent = deleteLast;
- 
-        if (this.operator !== undefined) return this.number2 = screen.textContent;
-        else {
-            console.log("pase por aqui")
-            return this.number1 = screen.textContent
-        };
+        this.currentNumber = this.currentNumber.toString().slice(0, -1);  
+    },
+
+    switch() {
+        this.currentNumber = this.currentNumber * -1;
     },
 
     calculate() {
-        console.log( `number1: ${this.number1}`)
-        console.log( `number2: ${this.number2}`)
-        console.log( `operator: ${this.operator}`)
+        if (this.currentNumber === "" || this.previousNumber === "") return;
 
-        if (this.number1 === "" || this.number2 === "") return;
-
-        this.number1 = parseFloat(this.number1);
-        this.number2 = parseFloat(this.number2);
+        this.currentNumber = parseFloat(this.currentNumber);
+        this.previousNumber = parseFloat(this.previousNumber);
 
         switch (this.operator) {
             case "+":
-                this.result = this.number1 + this.number2;
+                this.result = this.previousNumber + this.currentNumber;
                 break;
             case "-":
-                this.result = this.number1 - this.number2;
+                this.result = this.previousNumber - this.currentNumber;
                 break;
             case "x":
-                this.result = this.number1 * this.number2;
+                this.result = this.previousNumber * this.currentNumber;
                 break;
             case "÷":
-                this.result = this.number1 / this.number2;
+                this.result = this.previousNumber / this.currentNumber;
                 break;
             default:
                 break;
-        }
-
-        this.result = this.result.toFixed(2);
-
-        if(this.result.indexOf(".00") !== -1) parseInt(this.result);
-
-        previous.textContent = "";
-        screen.textContent = this.result;
-        this.number1 = this.result;
-        this.number2 = "";
+        };
+       
+        let resultString = this.result.toString();
+        if (resultString.indexOf(".") !== -1) {
+            let decimalPoint = resultString.indexOf(".");
+            let decimals = resultString.slice(decimalPoint);
+            if (decimals.length > 10) {
+                let decimalsNumber = Number(decimals);
+                let decimalsRoundedString = decimalsNumber.toFixed(9);
+                let decimalsRounded = Number(decimalsRoundedString); 
+                
+                this.result = parseInt(this.result) + decimalsRounded;
+            };
+        };
+                
+        this.currentNumber = this.result;
+        this.previousNumber = "";
         this.operator = undefined;
     },
 };
